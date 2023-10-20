e945151ec4c12017107d06665d7619065d9ecf09
duongherotey <nak.duongherotey@ecoinsoft.com>
2023-10-20 15:31:49 +0700

    add image profile

--- lib/screen.dart
+++ lib/screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive_project_tests/main.dart';
@@ -19,33 +20,46 @@ class _MyBooksState extends State<MyBooks> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Hive DB"),
+        title: const Text("Hive DB Testing"),
       ),
       body: ValueListenableBuilder(
         valueListenable: Hive.box<Employee>('booksbox').listenable(),
         builder: (context, Box<Employee> box, _) {
           if (box.values.isEmpty) {
-            return const Center(child: Text("No Books"));
+            return const Center(child: Text("No Person Info"));
           } else {
             return ListView.separated(
               itemCount: box.values.length,
               itemBuilder: (context, index) {
                 var result = box.getAt(index);
-
                 return Card(
                   child: ListTile(
+                    titleTextStyle:
+                        const TextStyle(fontSize: 20, color: Colors.blue),
+                    subtitleTextStyle:
+                        const TextStyle(fontSize: 10, color: Colors.blueGrey),
+                    leading: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.blueAccent,
+                          image: DecorationImage(
+                              image: AssetImage('asset/profile_image.png'),
+                              fit: BoxFit.fill)),
+                    ),
                     title: Padding(
                       padding: const EdgeInsets.only(top: 2),
                       child: Text(
                         result!.name!,
-                        style: TextStyle(color: Colors.black),
+                        // style: TextStyle(color: Colors.black),
                       ),
                     ),
                     subtitle: Text(result.position!),
                     trailing: Padding(
                       padding: const EdgeInsets.only(left: 10, top: 10),
                       child: Container(
-                        width: 50,
+                        width: 60,
                         height: 20,
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.start,
@@ -53,13 +67,13 @@ class _MyBooksState extends State<MyBooks> {
                           children: [
                             Container(
                               alignment: Alignment.centerLeft,
-                              width: 20,
+                              width: 35,
                               height: 15,
                               child: Padding(
                                 padding: const EdgeInsets.only(right: 10),
                                 child: Text(
                                   result.sex!,
-                                  style: TextStyle(color: Colors.black),
+                                  style: const TextStyle(color: Colors.brown),
                                 ),
                               ),
                             ),
@@ -76,6 +90,8 @@ class _MyBooksState extends State<MyBooks> {
                         ),
                       ),
                     ),
+                    onTap: () {
+                    },
                   ),
                 );
               },
@@ -98,7 +114,7 @@ class _MyBooksState extends State<MyBooks> {
         context: context,
         builder: (context) {
           return AlertDialog(
-            title: const Text("New book"),
+            title: const Text("Add New Employee"),
             content: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
@@ -129,7 +145,6 @@ class _MyBooksState extends State<MyBooks> {
                             sex: _sex.text,
                             position: _position.text,
                           ));
-
                       Navigator.pop(context);
                     },
                     child: const Text("Add")),
